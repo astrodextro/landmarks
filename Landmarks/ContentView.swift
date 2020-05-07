f87b4c37485aa4fc8dba93dd1da8e7fa776b4ca7
Felix Jimoh <astrodextro@gmail.com>
2020-05-07 13:11:24 +0100

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Doci Healthcare on 07/05/2020.
+//  Copyright © 2020 Sopht Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
